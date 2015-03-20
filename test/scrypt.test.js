02956ca6547efdab5e76406d267b329ad321d76b
Daniel Cousens <github@dcousens.com>
2015-03-20 12:04:26 +1100

    adopt standard style

--- test/scrypt.test.js
+++ test/scrypt.test.js
@@ -1,23 +1,25 @@
+/* global describe, it */
+
 var assert = require('assert')
 var scrypt = require('../')
 
 var fixtures = require('./fixtures/scrypt')
 
-//some tests params from: https://github.com/barrysteyn/node-scrypt/blob/master/tests/scrypt-tests.js
-//also: https://tools.ietf.org/html/draft-josefsson-scrypt-kdf-00
-//note, that params are wrong on here https://tools.ietf.org/html/draft-josefsson-scrypt-kdf-00 see: 
+// some tests params from: https://github.com/barrysteyn/node-scrypt/blob/master/tests/scrypt-tests.js
+// also: https://tools.ietf.org/html/draft-josefsson-scrypt-kdf-00
+// note, that params are wrong on here https://tools.ietf.org/html/draft-josefsson-scrypt-kdf-00 see:
 // => https://twitter.com/dchest/status/247734446881640448
 
-describe('scrypt', function() {
-  describe('> when valid', function() {
-    fixtures.valid.forEach(function(f) {
-      if (f.skip) return //impractical to run most times
-      it('should compute for ' + f.description, function() {
+describe('scrypt', function () {
+  describe('> when valid', function () {
+    fixtures.valid.forEach(function (f) {
+      if (f.skip) return // impractical to run most times
+
+      it('should compute for ' + f.description, function () {
         var data = scrypt(f.key, f.salt, f.iterations, f.memory, f.parallel, f.keyLen)
+
         assert.equal(data.toString('hex'), f.result)
       })
     })
   })
 })
-
-
